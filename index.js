d9d7fb9563e9b40db08abe85b07f230cc779f1bd
kimsj1993 <kimsj1993@tamu.edu>
2019-05-22 02:10:54 -0500

    Simple Refactoring

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/key');
-
+require('./services/passport');
 
 const app = express();
 
-// Register new strategy to the passport
-// Create new instance of the Google passport strategy
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
-        
-    })
-);
+require('./routes/authRoutes')(app);
 
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
- 
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);

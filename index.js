9be8006ffb310b6fab8daae7956832f4f7f78f0b
kimsj1993 <kimsj1993@tamu.edu>
2019-05-27 00:16:47 -0500

    settup for mongoose for new user

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/key')
+require('./models/User')
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
+
+
 
 const app = express();
 
